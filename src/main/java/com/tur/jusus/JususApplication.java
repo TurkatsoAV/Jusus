34202e3746fe341a1fcac7822d38aa1835e6a993
OUT-Turkatso-AV <Turkatso.A.V@sberbank.ru>
2020-09-24 16:59:14 +0300

    init spring boot application

---
+++ src/main/java/com/tur/jusus/JususApplication.java
@@ -0,0 +1,13 @@
+package com.tur.jusus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JususApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JususApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tur/jusus/JususApplicationTests.java
@@ -0,0 +1,8 @@
+package com.tur.jusus;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JususApplicationTests {
+
+}
