cd7e6d51aff2f285dc1d60440b5c08f6c416f0ff
OUT-Turkatso-AV <Turkatso.A.V@sberbank.ru>
2020-09-25 13:31:45 +0300

    реализация метода getAuthorities

--- src/main/java/com/tur/jusus/entity/User.java
+++ src/main/java/com/tur/jusus/entity/User.java
@@ -3,11 +3,13 @@ package com.tur.jusus.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "USERS")
@@ -33,7 +35,9 @@ public class User extends Base implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
     }
 
     @Override
