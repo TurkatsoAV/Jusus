55256fccb121745957234bbb168484982921c36a
OUT-Turkatso-AV <Turkatso.A.V@sberbank.ru>
2020-09-25 12:02:43 +0300

    определение базового класса как суперкласса для сущностей jpa

--- src/main/java/com/tur/jusus/entity/Base.java
+++ src/main/java/com/tur/jusus/entity/Base.java
@@ -2,9 +2,14 @@ package com.tur.jusus.entity;
 
 import lombok.Data;
 
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
 @Data
+@MappedSuperclass
 public abstract class Base {
+
+    @Id
     private UUID uuid;
 }
