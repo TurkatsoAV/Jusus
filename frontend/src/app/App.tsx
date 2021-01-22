90a77ba4e2e3045241f98357f2b26256407555c3
turkatso alexander <turkatso.alexander@gmail.com>
2021-01-22 21:33:15 +0300

    include react router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Login from "../login/Login";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from "../dashboard/Dashboard";
+import {BrowserRouter, Route} from "react-router-dom";
 
-const App: React.FC = () => <Login />;
+const App: React.FC = () =>
+    <BrowserRouter>
+        <Route path="/login" component={Login}/>
+        <Route path="/dashboard" component={Dashboard}/>
+    </BrowserRouter>;
 export default App;
---
+++ frontend/src/dashboard/Dashboard.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Dashboard : React.FC = () => <div>Dashboard</div>;
+
+export default Dashboard;
