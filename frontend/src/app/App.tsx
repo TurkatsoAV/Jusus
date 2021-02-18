e8f87deabb597175558daa5c8392c145368372bd
TurkatsoAV <turkatso.alexander@gmail.com>
2021-02-18 23:41:28 +0300

    refactoring code

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import Login from "../login/Login";
-import Dashboard from "../dashboard/Dashboard";
-import {BrowserRouter, Route} from "react-router-dom";
+import React from "react"
+import { Dashboard, Login } from "../screen"
+import { HashRouter, Route } from "react-router-dom"
 
-const App: React.FC = () =>
-    <BrowserRouter>
-        <Route path="/login" component={Login}/>
-        <Route path="/dashboard" component={Dashboard}/>
-    </BrowserRouter>;
-export default App;
+export const App: React.FC = () => (
+    <HashRouter>
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+    </HashRouter>
+)
